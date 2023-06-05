755210a01783fda99412c360209b6a70ba83810e
Jorge Lucas <jorgelucas.gs1998@gmail.com>
2023-06-05 17:56:01 -0300

    sessão de portfolio pronta!

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+import bitCent from '../assets/projects/bit-cent.png'
+import bubbleCoffee from '../assets/projects/bubble-coffee.png'
+import dsfBank from '../assets/projects/dsf-bank.png'
+
+export const data=[
+    {
+        id:1,
+        name:"ReactJS & Tailwind",
+        image:bitCent,
+        github:"https://github.com/JorgeGS1998/BitCent",
+        live:"https://bit-cent-jorgegs1998.vercel.app/",
+    },
+    {
+        id:1,
+        name:"NextJs JS & Sass",
+        image:bubbleCoffee,
+        github:"https://github.com/JorgeGS1998/Bubble-Coffee",
+        live:"https://bubble-coffee-s.vercel.app/",
+    },
+    {
+        id:1,
+        name:"React JS Application",
+        image:dsfBank,
+        github:"https://github.com/JorgeGS1998/DSF-Bank-",
+        live:"https://dsf-bank.vercel.app/",
+    },
+
+]
