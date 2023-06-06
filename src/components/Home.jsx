f37cab728b030274fb8dd48aafbae1fe0a5581fb
Jorge Lucas <jorgelucas.gs1998@gmail.com>
2023-06-06 17:32:23 -0300

    home mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,8 @@ import {FiDownload} from 'react-icons/fi';
 
 const Home = () => {
   return (
-    <div id='home' className='w-full h-screen  bg-[#0a192f]  text-gray-100'>
-      <div className='max-w-[1000px] w-full h-full mx-auto  flex justify-center items-center'>
+    <div id='home' className='w-full h-screen bg-[#0a192f] text-gray-100'>
+      <div className='max-w-[1000px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
           <p className='text-pink-600'>Olá, meu nome é</p>
           <h1 className='py-4 text-4xl sm:text-7xl font-bold text-[#ccd6f6]'> Jorge Lucas </h1>
@@ -14,9 +14,7 @@ const Home = () => {
           </p>
           <div className='flex items-center gap-5 max-w-[330px] py-4'>
             <a
-              href='https://www.linkedin.com/in/clint-briley-50056920a/'
-              target='_blank'
-              rel='noreferrer'
+              href='https://github.com/JorgeGS1998' target='_blank' rel='noreferrer'
             >
               <div className='rounded-full bg-pink-600 p-5 cursor-pointer hover:scale-110 ease-in duration-300'>
                 <FaLinkedinIn />
